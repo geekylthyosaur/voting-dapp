3704e3e3566380262aa80294f17e6e3bac4b4265
geekylthyosaur <geekylthyosaur@gmail.com>
2024-08-20 23:48:48 +0300

    Hello, world!

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+use solana_program::{account_info::AccountInfo, entrypoint::ProgramResult, pubkey::Pubkey};
+
+solana_program::entrypoint!(process_instruction);
+
+pub fn process_instruction(
+    _program_id: &Pubkey,
+    _accounts: &[AccountInfo],
+    _instruction_data: &[u8],
+) -> ProgramResult {
+    solana_program::msg!("Hello, world!");
+
+    Ok(())
+}
