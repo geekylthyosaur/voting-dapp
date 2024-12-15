3267b237ccd4d3b7accc78923bc2e1b9ec2f5ac9
geekylthyosaur <geekylthyosaur@gmail.com>
2024-12-15 22:19:31 +0200

    Minimal working program and client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,66 +1,109 @@
 use std::str::FromStr;
 
-use borsh::{BorshDeserialize, BorshSerialize};
+use borsh::BorshDeserialize;
 use solana_client::rpc_client::RpcClient;
 use solana_sdk::{
     commitment_config::CommitmentConfig,
     instruction::{AccountMeta, Instruction},
     pubkey::Pubkey,
-    signature::{read_keypair_file, Keypair, Signer},
-    system_program,
+    signature::{read_keypair_file, Signer},
+    system_instruction,
     transaction::Transaction,
 };
+use state::VoteType;
 
-#[derive(BorshDeserialize, BorshSerialize, Debug)]
-#[non_exhaustive]
-enum InstructionData {
-    CreatePoll(Poll),
+mod state {
+    use borsh::{BorshDeserialize, BorshSerialize};
+
+    #[derive(BorshDeserialize, BorshSerialize, Clone, Copy, Debug, Default)]
+    pub struct Poll {
+        gm: u64,
+        gn: u64,
+    }
+
+    impl Poll {
+        pub const SIZE: usize = 16;
+    }
+
+    #[derive(BorshDeserialize, BorshSerialize, Clone, Copy, Debug)]
+    pub enum VoteType {
+        GM,
+        GN,
+    }
 }
 
-#[derive(BorshDeserialize, BorshSerialize, Debug)]
-struct Poll {
-    question: String,
+mod instruction {
+    use borsh::{BorshDeserialize, BorshSerialize};
+
+    use super::state::VoteType;
+
+    #[derive(BorshDeserialize, BorshSerialize, Debug)]
+    #[non_exhaustive]
+    pub enum Instruction {
+        Vote(VoteType),
+    }
 }
 
 fn main() {
-    let program_id = Pubkey::from_str(std::env!("PROGRAM_ID")).unwrap();
+    let vote_type = parse_args().expect("Error: expected argument GM/GN");
 
-    let connection =
-        RpcClient::new_with_commitment("http://localhost:8899", CommitmentConfig::confirmed());
+    let program_id = Pubkey::from_str(env!("PROGRAM_ID")).unwrap();
+    let rpc_url = "http://localhost:8899";
+    let rpc_client = RpcClient::new_with_commitment(rpc_url, CommitmentConfig::confirmed());
+    let poll = read_keypair_file("./poll.json").unwrap();
+    let poll_id = poll.pubkey();
+    let payer = read_keypair_file("/home/dmytro/.config/solana/id.json").unwrap();
+    let payer_id = payer.pubkey();
 
-    let keypair = read_keypair_file("/home/dmytro/.config/solana/id.json").unwrap();
+    if rpc_client.get_account(&poll_id).is_err() {
+        println!("Poll does not exist, creating...");
 
-    let blockhash_info = connection.get_latest_blockhash().unwrap();
+        let rent_exemtion = rpc_client
+            .get_minimum_balance_for_rent_exemption(state::Poll::SIZE)
+            .unwrap();
+        let ix_create = system_instruction::create_account(
+            &payer_id,
+            &poll_id,
+            rent_exemtion,
+            state::Poll::SIZE as u64,
+            &program_id,
+        );
 
-    let poll = Poll {
-        question: "How are you doing?".to_string(),
-    };
-    let instruction_data = InstructionData::CreatePoll(poll);
-    let mut bytes = Vec::new();
-    instruction_data.serialize(&mut bytes).unwrap();
+        let latest_blockhash = rpc_client.get_latest_blockhash().unwrap();
+        let tx = Transaction::new_signed_with_payer(
+            &[ix_create],
+            Some(&payer_id),
+            &[&payer, &poll],
+            latest_blockhash,
+        );
+        rpc_client.send_and_confirm_transaction(&tx).unwrap();
 
-    let payer_pubkey = keypair.pubkey();
-    let new_account = Keypair::new();
-    let system_program_id = system_program::id();
+        println!("Poll created: {}", poll_id);
+    } else if let Ok(data) = rpc_client.get_account_data(&poll_id) {
+        let poll = state::Poll::try_from_slice(&data).unwrap();
+        println!("{poll:?}");
+    }
 
-    let accounts = vec![
-        AccountMeta::new(payer_pubkey, true),
-        AccountMeta::new(new_account.pubkey(), true),
-        AccountMeta::new_readonly(system_program_id, false),
-    ];
+    let vote = instruction::Instruction::Vote(vote_type);
+    let accounts = vec![AccountMeta::new(poll_id, false)];
+    let ix = Instruction::new_with_borsh(program_id, &vote, accounts);
 
-    let mut transaction = Transaction::new_with_payer(
-        &[Instruction::new_with_bytes(program_id, &bytes, accounts)],
-        Some(&keypair.pubkey()),
-    );
+    let latest_blockhash = rpc_client.get_latest_blockhash().unwrap();
+    let tx = Transaction::new_signed_with_payer(&[ix], Some(&payer_id), &[payer], latest_blockhash);
 
-    transaction
-        .try_sign(&[&keypair, &new_account], blockhash_info)
-        .unwrap();
+    let tx_sig = rpc_client.send_and_confirm_transaction(&tx).unwrap();
 
-    let tx_hash = connection
-        .send_and_confirm_transaction(&transaction)
-        .unwrap();
+    println!("https://explorer.solana.com/tx/{}?cluster=custom", tx_sig);
+}
 
-    println!("https://explorer.solana.com/tx/{}?cluster=custom", tx_hash);
+fn parse_args() -> Option<VoteType> {
+    if let Some(s) = std::env::args().skip(1).next().as_ref() {
+        match s.as_str() {
+            "gm" => Some(VoteType::GM),
+            "gn" => Some(VoteType::GN),
+            _ => None,
+        }
+    } else {
+        None
+    }
 }
--- program/src/lib.rs
+++ program/src/lib.rs
@@ -1,75 +1,122 @@
-use std::str::FromStr;
+mod entrypoint {
+    use super::instruction;
 
-use borsh::{BorshDeserialize, BorshSerialize};
-use solana_program::{
-    account_info::{next_account_info, AccountInfo},
-    entrypoint::ProgramResult,
-    native_token::LAMPORTS_PER_SOL,
-    program,
-    pubkey::Pubkey,
-    system_instruction,
-};
-
-solana_program::entrypoint!(process_instruction);
-
-#[derive(BorshDeserialize, Debug)]
-#[non_exhaustive]
-enum InstructionData {
-    CreatePoll(Poll),
-}
-
-#[derive(BorshDeserialize, BorshSerialize, Debug)]
-struct Poll {
-    question: String,
-}
-
-pub fn process_instruction(
-    _program_id: &Pubkey,
-    accounts: &[AccountInfo],
-    instruction_data: &[u8],
-) -> ProgramResult {
-    let instruction = InstructionData::try_from_slice(instruction_data).unwrap();
-
-    match instruction {
-        InstructionData::CreatePoll(poll) => {
-            let accounts_iter = &mut accounts.iter();
-            let payer = next_account_info(accounts_iter)?;
-            let new_account = next_account_info(accounts_iter)?;
-            let system_program = next_account_info(accounts_iter)?;
-            create_poll(payer, new_account, system_program, &poll)?
-        }
+    use borsh::BorshDeserialize;
+    use solana_program::{
+        account_info::AccountInfo, entrypoint::ProgramResult, msg, pubkey::Pubkey,
     };
 
-    Ok(())
+    solana_program::entrypoint!(process_instruction);
+
+    pub fn process_instruction(
+        program_id: &Pubkey,
+        accounts: &[AccountInfo],
+        instruction_data: &[u8],
+    ) -> ProgramResult {
+        let instruction = instruction::Instruction::try_from_slice(instruction_data).unwrap();
+
+        msg!(
+            "{{ ProgramId({}), {}, {:?} }}",
+            program_id,
+            accounts.len(),
+            instruction
+        );
+
+        super::processor::process_instruction(program_id, accounts, instruction)?;
+
+        Ok(())
+    }
 }
 
-fn create_poll<'a>(
-    payer: &AccountInfo<'a>,
-    new_account: &AccountInfo<'a>,
-    system_program: &AccountInfo<'a>,
-    poll: &Poll,
-) -> ProgramResult {
-    let mut account_data = Vec::new();
-    poll.serialize(&mut account_data).unwrap();
-    let account_size = account_data.len() as u64;
-    let account_owner = Pubkey::from_str(std::env!("PROGRAM_ID")).unwrap();
+mod instruction {
+    use borsh::BorshDeserialize;
 
-    program::invoke(
-        &system_instruction::create_account(
-            payer.key,
-            new_account.key,
-            LAMPORTS_PER_SOL,
-            account_size,
-            &account_owner,
-        ),
-        &[payer.clone(), new_account.clone(), system_program.clone()],
-    )?;
+    use super::state::VoteType;
 
-    new_account.data.borrow_mut()[..account_data.len()].copy_from_slice(&account_data);
-
-    Ok(())
+    #[derive(BorshDeserialize, Debug)]
+    #[non_exhaustive]
+    pub enum Instruction {
+        Vote(VoteType),
+    }
 }
 
-fn vote(voter: &AccountInfo, poll: &Pubkey) -> ProgramResult {
-    Ok(())
+mod state {
+    use borsh::{BorshDeserialize, BorshSerialize};
+
+    #[derive(BorshDeserialize, BorshSerialize, Clone, Copy, Debug)]
+    pub struct Poll {
+        gm: u64,
+        gn: u64,
+    }
+
+    impl Poll {
+        pub const SIZE: usize = 16;
+
+        pub fn vote(&mut self, vote: VoteType) {
+            match vote {
+                VoteType::GM => self.gm += 1,
+                VoteType::GN => self.gn += 1,
+            }
+        }
+    }
+
+    #[derive(BorshDeserialize, BorshSerialize, Clone, Copy, Debug)]
+    pub enum VoteType {
+        GM,
+        GN,
+    }
+}
+
+mod processor {
+    use borsh::{BorshDeserialize, BorshSerialize};
+    use solana_program::{
+        account_info::{next_account_info, AccountInfo},
+        entrypoint::ProgramResult,
+        msg,
+        program_error::ProgramError,
+        pubkey::Pubkey,
+    };
+
+    use crate::{instruction::Instruction, state::Poll};
+
+    pub fn process_instruction(
+        program_id: &Pubkey,
+        accounts: &[AccountInfo],
+        instruction: Instruction,
+    ) -> ProgramResult {
+        let accounts_iter = &mut accounts.iter();
+
+        let account = next_account_info(accounts_iter)?;
+
+        if account.owner != program_id {
+            msg!(
+                "Poll account is not owned by the program ({} != {})",
+                account.owner,
+                program_id
+            );
+            return Err(ProgramError::IncorrectProgramId);
+        }
+
+        if account.data_len() < Poll::SIZE {
+            msg!(
+                "Poll account too small ({} < {})",
+                account.data_len(),
+                Poll::SIZE
+            );
+            return Err(ProgramError::InvalidAccountData);
+        }
+
+        let mut poll = Poll::try_from_slice(&account.data.borrow()).unwrap();
+
+        match instruction {
+            Instruction::Vote(t) => {
+                poll.vote(t);
+            }
+        };
+
+        poll.serialize(&mut &mut account.data.borrow_mut()[..])
+            .unwrap();
+
+        Ok(())
+    }
 }
